2f3a932e0238c33406da65b3716bcf054c5947f0
shinji.sakaguchi <shinji.sakaguchi@mixi.co.jp>
2024-11-19 03:39:56 +0000

    update: assign max_tokens

--- lecture_function_calling/simple_chat.py
+++ lecture_function_calling/simple_chat.py
@@ -13,23 +13,43 @@ load_dotenv()
 
 
 def chat_openai(messages: list[AnyMessage]) -> None:
-    # モデル名は https://platform.openai.com/docs/models 参照
-    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.5)
+    llm = ChatOpenAI(
+        # モデル名は https://platform.openai.com/docs/models 参照
+        model="gpt-4o-mini",
+        # 0.2だと正しいけれども多様性が少なめ、0.5はバランスが良い、0.9だと多様性が高いけれども誤りが出てくる と言われています。
+        temperature=0.5,
+        # 出力時の最大トークン数。大体 *0.7倍した数値が出力日本語の文字数です。この場合、140文字くらい
+        max_tokens=200,
+    )
     ai_message: AIMessage = llm.invoke(input=messages)
     print(ai_message)
     print(ai_message.content)
 
 
 def chat_anthropic(messages: list[AnyMessage]) -> None:
-    # モデル名は、 https://docs.anthropic.com/en/docs/about-claude/models 参照
-    llm = ChatAnthropic(model="claude-3-5-haiku-20241022", temperature=0.5)
+    llm = ChatAnthropic(
+        # モデル名は、 https://docs.anthropic.com/en/docs/about-claude/models 参照
+        model="claude-3-5-haiku-20241022",
+        # https://docs.anthropic.com/en/docs/resources/glossary#temperature
+        temperature=0.5,
+        # 出力時の最大トークン数。多分 *0.7倍した数値が出力日本語の文字数です。この場合、140文字くらい
+        max_tokens_to_sample=200,
+    )
     ai_message: AIMessage = llm.invoke(input=messages)
     print(ai_message)
     print(ai_message.content)
 
 
 def chat_gemini(messages: list[AnyMessage]) -> None:
-    llm = ChatVertexAI(model="gemini-1.5-flash-001", temperature=0.5)
+    llm = ChatVertexAI(
+        # モデル名は  https://cloud.google.com/vertex-ai/generative-ai/docs/learn/models 参照のこと
+        model="gemini-1.5-flash-002",
+        # https://cloud.google.com/vertex-ai/generative-ai/docs/learn/prompts/adjust-parameter-values#temperature
+        temperature=0.5,
+        # https://cloud.google.com/vertex-ai/generative-ai/docs/learn/prompts/adjust-parameter-values#max-output-tokens
+        # 1word=4token とあるが、おそらく英語の場合
+        max_tokens=200,
+    )
     ai_message: AIMessage = llm.invoke(input=messages)
     print(ai_message)
     print(ai_message.content)
