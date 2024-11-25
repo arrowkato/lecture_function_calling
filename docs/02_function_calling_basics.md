f7562267212a2b04ed525d4cfb02111371fbd9be
shinji.sakaguchi <shinji.sakaguchi@mixi.co.jp>
2024-11-25 04:12:45 +0000

    update: function callingの基本的な動作を解説

---
+++ docs/02_function_calling_basics.md
@@ -0,0 +1,30 @@
+
+
+
+# 2.1 用語
+- OpenAI: [function calling](https://platform.openai.com/docs/guides/function-calling)
+- Anthropic: [tool use](https://docs.anthropic.com/en/docs/build-with-claude/tool-use)
+- Google: [function calling](https://cloud.google.com/vertex-ai/generative-ai/docs/multimodal/function-calling)
+- LangChain: [tool calling](https://python.langchain.com/docs/how_to/tool_calling/)
+
+目的は同じですが、各製品で表記揺れがあります。
+
+講義では、LangChainの場合は、tool callingと呼ぶつもりですが、function callingとtool callingは同じものを指すと思ってください
+
+# 2.2 モチベーション
+
+> Function callingは、2023年6月にChat Completions APIに追加された機能です。簡単に言えば、 利用可能な関数を LLM に伝えておいて 、LLMに「関数を使いたい」という判断をさせる機能です(LLMが関数を実行するわけではなく、LLM は「関数を使いたい」という応答を返してくるだけです)
+
+LangChainとLangGraphによるRAG_AIエージェント実践入門 より引用
+
+下図の1.に当たります。
+
+![状態遷移](./img/function-calling-diagram-ja.png)
+
+https://platform.openai.com/docs/guides/function-calling#lifecycle の図を日本語に翻訳
+
+
+
+# ref
+- https://platform.openai.com/docs/guides/function-calling
+- https://zenn.dev/pharmax/articles/1b351b730eef61#tool%E3%81%AE%E4%BD%9C%E3%82%8A%E6%96%B9
--- lecture_function_calling/function_calling_basic.py
+++ lecture_function_calling/function_calling_basic.py
@@ -1,9 +1,11 @@
 """よくある四則演算のサンプル。
 https://python.langchain.com/docs/how_to/tool_calling/#pydantic-class
 
-tool callの日本語の解説
+tool callの日本語の解説。ただし、LangGraphで使うことを想定した解説
 https://zenn.dev/pharmax/articles/1b351b730eef61
 """
+
+import json
 
 from langchain_core.messages import AIMessage, AnyMessage, HumanMessage, SystemMessage
 from langchain_core.tools import tool
@@ -11,6 +13,7 @@ from langchain_openai import ChatOpenAI
 
 
 # これは、tool callingで呼び出す用の関数その1
+# 関数名、引数、返り値、docstring を参照して、LLMがこの関数を使うべきかを決めるので、きちんと書くこと
 @tool
 def add(a: float | int, b: float | int) -> int:
     """Add two real number.
@@ -23,6 +26,7 @@ def add(a: float | int, b: float | int) -> int:
 
 
 # これは、tool callingで呼び出す用の関数その2
+# 関数名、引数、返り値、docstring を参照して、LLMがこの関数を使うべきかを決めるので、きちんと書くこと
 @tool
 def multiply(a: float | int, b: float | int) -> int:
     """Multiply two real number.
@@ -34,31 +38,71 @@ def multiply(a: float | int, b: float | int) -> int:
     return a * b
 
 
-def use_tool_call() -> None:
-    llm = ChatOpenAI(model="gpt-4o", temperature=0.0)
-    llm_with_tools = llm.bind_tools(tools=[add, multiply])
-    system_msg = SystemMessage(content="あなたは、親切な数学教師です。")
-    human_msg = HumanMessage(content="12 * 1.5 + 4 はいくつですか。")
-    msgs: list[AnyMessage] = [system_msg, human_msg]
+def _show_response(response: AnyMessage, prefix: str, delimiter: str = "-") -> None:
+    response_dict = {
+        "content": response.content,
+        "additional_kwargs": response.additional_kwargs,
+        "tool_calls": response.tool_calls if hasattr(response, "tool_calls") else None,
+    }
+    print(delimiter * 50)
+    print(f"{prefix}\n{json.dumps(response_dict, indent=4, ensure_ascii=False,)}")
+    print()
+    print()
 
-    # このresponseは、どの関数を使うべきかを返却しています。addとmultiply の実行はまだ、というかLLM自体はコードの実行はできません。
+
+def _show_responses(
+    responses: list[AnyMessage],
+    prefix: str,
+    delimiter: str = "-",
+) -> None:
+    print("=" * 50)
+    for i, response in enumerate(responses):
+        _show_response(response, prefix=f"{prefix} {i}", delimiter=delimiter)
+
+
+def use_tool_call(question: str) -> None:
+    # いつものLLMの定義
+    llm = ChatOpenAI(model="gpt-4o", temperature=0.0)
+    # step0使う可能性のある関数(addとmultiply)を登録
+    llm_with_tools = llm.bind_tools(tools=[add, multiply])
+
+    msgs: list[AnyMessage] = [
+        SystemMessage(content="あなたは、親切な数学教師です。"),
+        HumanMessage(content=question),
+    ]
+
+    # step1, step2, step3
+    # このresponseは、どの関数を使うべきかを返却しています。
+    # addとmultiply の実行はまだ、というかLLM自体はコードの実行はできません。
     response: AIMessage = llm_with_tools.invoke(input=msgs)
+
+    # responseの中身を見てみる
+    _show_response(response, "step3 result")
 
     # 後でもう一回使うので、msgsに追加
     msgs.append(response)
 
-    # response.tool_calls[0]["args"]
+    _show_responses(msgs, "before step4", delimiter="-")
+
+    # step4
     for tool_call in response.tool_calls:
         selected_tool = {"add": add, "multiply": multiply}[tool_call["name"].lower()]
-        tool_msg = selected_tool.invoke(tool_call)
+        tool_msg = selected_tool.invoke(tool_call)  # add or multiplyの実行
         msgs.append(tool_msg)
 
-    # このタイミングで、ブレークポイントとを張って、msgsの中身を見てみましょう
+    # このタイミングで、ブレークポイントを張って、msgsの中身を見てみましょう
+    _show_responses(msgs, "after step4", delimiter="-")
 
-    # tool_callを使って、計算した結果を踏まえての最終結果
+    # step5 tool_callを使って、計算した結果を踏まえての最終結果
     final_response: AIMessage = llm_with_tools.invoke(msgs)
+    print("=" * 50)
     print(final_response.content)
+
+    _show_response(response=final_response, prefix="final result detail")
 
 
 if __name__ == "__main__":
-    use_tool_call()
+    question1 = "929801.2 * 380.29  はいくつですか。"  # 353594098.348  桁数の少ない四則演算だとLLMは普通に答えるので
+    question2 = "390.32 + 84.41 はいくつですか。"
+    use_tool_call(question=question1)
+    # use_tool_call(question=question2)
